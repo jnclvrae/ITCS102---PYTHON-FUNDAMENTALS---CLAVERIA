812c18070dc03e1d55dd85a6466353b8e0ead7c4
Nace <clvrnc17@gmail.com>
2025-08-22 20:58:52 +0800

    Update activity6.py

--- activity6.py
+++ activity6.py
@@ -1,7 +1,15 @@
-something = eval(input("Input something --> "))
+n1 = eval(input("Enter the first number: "))
+n2 = eval(input("Enter the second number: "))
 
-print("The data type of something is", type(something))
+s = n1 + n2
+d = n1 - n2
+p = n1 * n2
+q = n1 / n2
 
-answer = 6 + something
-
-print("The answer is", answer)
+print("\nThe sum of", n1, "and", n2, "is", s)
+print("The difference of", n1, "and", n2, "is", d)
+print("The product of", n1, "and", n2, "is", p)
+print("The quotient of", n1, "and", n2, "is", q)
+print(n1, "exponent by", n2, "is", n1 ** n2)
+print("The remainder of", n1, "and", n2, "is", n1 % n2)
+print("The floor division of", n1, "and", n2, "is", n1 // n2)
