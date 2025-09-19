91de606a29491831baa39735d03d127c7d1bea4a
Nace <clvrnc17@gmail.com>
2025-09-19 11:27:45 +0800

    ITCS-102 ACTIVITIES
    Activities done on Sept., 18, 2025.

---
+++ activity15.py
@@ -0,0 +1,9 @@
+odd_sum = 0
+
+for i in range(1,11,1):
+    num = eval(input(f"{i} - Enter a number --> "))
+
+    if num % 2 == 1:
+        odd_sum += num
+
+print(f"\nThe sum of all the ODD numbers given is {odd_sum}.")
