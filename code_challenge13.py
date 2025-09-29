846b2f619d83e8afc5b5b7c5b3f0230a1523655f
Nace <clvrnc17@gmail.com>
2025-09-29 17:51:02 +0800

    CODE CHALLENGE 13
    Code challenge done on Sept. 29, 2025.

---
+++ code_challenge13.py
@@ -0,0 +1,58 @@
+print("\t\t\t\t     *",end=" ")
+for i in range(1,3,1):
+    for x in range(20, i, -1):
+        print(" ",end=" ")
+    for y in range(1, i, 1):
+        print("*",end=" ")
+    for z in range(1, i, 1):
+        print("*",end=" ")
+    print()
+    
+for i in range(3,1,-1):
+    for x in range(20, i, -1):
+        print(" ",end=" ")
+    for y in range(1, i, 1):
+        print("*",end=" ")
+    for z in range(1, i, 1):
+        print("*",end=" ")
+    print()
+    
+print("\t\t\t\t     *",end=" ")
+print("\n\t\t\t\t     *",end=" ")
+for i in range(1,11,1):
+    for x in range(20, i, -1):
+        print(" ",end=" ")
+    for y in range(1, i, 1):
+        print("*",end=" ")
+    for z in range(1, i, 1):
+        print("*",end=" ")
+    print()
+    
+print("\t\t\t\t     *",end=" ")
+for i in range(1,13,1):
+    for x in range(20, i, -1):
+        print(" ",end=" ")
+    for y in range(1, i, 1):
+        print("*",end=" ")
+    for z in range(1, i, 1):
+        print("*",end=" ")
+    print()
+    
+print("\t\t\t\t     *",end=" ")
+for i in range(1,15,1):
+    for x in range(20, i, -1):
+        print(" ",end=" ")
+    for y in range(1, i, 1):
+        print("*",end=" ")
+    for z in range(1, i, 1):
+        print("*",end=" ")
+    print()
+    
+for i in range(1,7,1):
+    for x in range(18, 1, -1):
+        print(" ",end=" ")
+    for y in range(1, 5, 1):
+        print("*",end=" ")
+    # for z in range(1, i, 1):
+    #     print("*",end=" ")
+    print()
